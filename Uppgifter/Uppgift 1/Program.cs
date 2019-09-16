fcd314aed82378db32b50edc3ed03f5ba4cc0de4
Neohack00 <neo@bylock.se>
2019-09-16 09:51:29 +0200

    uppgift 1 done and installed new project 'uppgift 2'

--- Uppgifter/Uppgift 1/Program.cs
+++ Uppgifter/Uppgift 1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Uppgift_1
 {
@@ -10,7 +11,42 @@ namespace Uppgift_1
     {
         static void Main(string[] args)
         {
+            WriteLine("Skriv in en mening");
+            string mening = ReadLine();
+            mening = mening + " ";
+            int ordLength = mening.Length;
+            int wordCount = 0;     
+            int checkpoint = 0;
+            int wordLen = 0;
+            
+            for (int i = 0; i < ordLength; i++)
+            {
+                
+                if (mening[i] == ' ' )
+                {
 
+                    wordCount++;
+                    wordLen = i - checkpoint;
+                    for(int y = checkpoint; y < i; y++)
+                    {
+                        char word = mening[y];
+                        Write(word);
+                        
+                    }
+                    checkpoint = i + 1;
+                    Write("   Antal karaktärer " + wordLen + "\n");
+                    
+           
+                }
+                
+            }
+
+
+            Write(" Antal ord :" + wordCount);
+
+
+            Read();
+            
         }
     }
 }
