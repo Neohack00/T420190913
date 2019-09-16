65baf876e2d022f5be3bf12177ded61f8eac3ad2
Neohack00 <neo@bylock.se>
2019-09-16 11:55:49 +0200

    Uppgift 2.1, input data done

--- Uppgifter/Uppgift 2/Program.cs
+++ Uppgifter/Uppgift 2/Program.cs
@@ -8,8 +8,16 @@ namespace Uppgift_2
 {
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
+            //Låt användaren mata in en mening.
+            Console.WriteLine("Skriv en mening");
+            string mening = Console.ReadLine();
+
+            //Skriv ut varje ord på en egen rad, i storleksordning(längd på ordet)
+            //Skriv ut varje ord på en egen rad, i omvänd ordning(baklänges)
+            Console.WriteLine(mening);
+            Console.Read();
         }
     }
 }
