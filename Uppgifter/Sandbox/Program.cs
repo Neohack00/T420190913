d0a69a001b9b50c3e49ec6f481855bc489ea2b04
Neohack00 <neo@bylock.se>
2019-09-17 12:53:45 +0200

    Testing Array list in sandbox

--- Uppgifter/Sandbox/Program.cs
+++ Uppgifter/Sandbox/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+
 
 namespace Sandbox
 {
@@ -10,6 +12,22 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
+         ArrayList Alist = new ArrayList();
+            Alist.Add("Hej");
+            Alist.Add("Neo");
+            Alist.Add("!");
+
+            Console.WriteLine(Alist);
+            Console.WriteLine("Alist");
+            Console.WriteLine(" Count:  {0}", Alist.Count);
+            Console.WriteLine(" Capacity:   {0} ", Alist.Capacity);
+            Console.WriteLine("Values:    ");
+            foreach(Object Sak in Alist)
+            {
+                Console.WriteLine("    {0}", Sak);
+            }
+            Console.ReadLine();
+
         }
     }
 }
