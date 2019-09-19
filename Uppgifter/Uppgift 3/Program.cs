9e3680313da2e3f6d221647a4bdbc0f51288a7d7
Neohack00 <neo@bylock.se>
2019-09-19 18:21:32 +0200

    Uppgift 3.2, matching function: working

--- Uppgifter/Uppgift 3/Program.cs
+++ Uppgifter/Uppgift 3/Program.cs
@@ -14,14 +14,50 @@ namespace Uppgift_3
             Console.WriteLine("Skriv in en mening");
             string mening = Console.ReadLine();
             string[] words = mening.Split();
-        //Skriv ut varje unikt ord på en egen rad, och ange hur ofta ordet förekommer(ex "och (2)")
-         if
-        //Skriv ut varje unikt ord på en egen rad, sorterat efter hur ofta ordet förekommer
+            //Skriv ut varje unikt ord på en egen rad, och ange hur ofta ordet förekommer(ex "och (2)")
+            int matchCount = 0;
+            System.Collections.ArrayList Blacklist = new System.Collections.ArrayList();
+            Blacklist.Add(" ");
+            bool ban = false;
+            foreach (string ord in words)
+             {
+
+                foreach (string Listedword in Blacklist)
+                {
+                    if (Listedword.Equals(ord) == true)
+                    {
+                        ban = true;
+                    }
+                }
+                if (ban != true)
+                {
+                    foreach (string CompareOrd in words)
+                    {
+
+                        if (CompareOrd.Equals(ord) == true)
+                        {
+                            matchCount++;
+
+                        }
+
+                    }
+
+                    Console.WriteLine("{0} {1}", ord, matchCount);
+                  
+                    Blacklist.Add(ord);
+                    matchCount = 0;
+
+                }
+                ban = false;
+
+            }
+
+            //Skriv ut varje unikt ord på en egen rad, sorterat efter hur ofta ordet förekommer
 
 
 
 
-
+            Console.Read();
         }
     }
 }
