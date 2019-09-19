e7cf8ef919d555f6646942d736e860379a3d00b1
Neohack00 <neo@bylock.se>
2019-09-19 17:21:32 +0200

    Restarting Uppgift 3, input function done

--- Uppgifter/Uppgift 3/Program.cs
+++ Uppgifter/Uppgift 3/Program.cs
@@ -9,44 +9,19 @@ namespace Uppgift_3
     class Program
     {
         static void Main(string[] args)
-        { 
-         
-
-            Console.WriteLine("skriv en mening");
+        {
+            //Låt användaren mata in en mening.
+            Console.WriteLine("Skriv in en mening");
             string mening = Console.ReadLine();
-            string[] words = mening.Split(' ');// Vi har en method som kan räkna orden samt array som förvara dem
-            ////Console.WriteLine(words.Length);//Test körnings utskrift
-   
-            Dictionary<string, int> OrdKollen = new Dictionary<string, int>();
-
-            for (int i = 0; i < words.Length; i++)
-            {
-                string Compare = words[i];
-                for (int x = 0; i < words.Length; x++)
-                {
-                    int matching = string.Compare(Compare, words[x]);
-                    if (matching != 0)
-                    {
-                        OrdKollen.Add(words[x], 1);
-                    }
-                    else if (matching == 0)
-                    {
-                        Dictionary<string, string>.ValueCollection valueColl = OrdKollen.Values;
-                        Console.WriteLine();
-                        foreach (string s in valueColl)
-                        {
-                            Console.WriteLine("Value = {0}", s);
-                        }
+            string[] words = mening.Split();
+        //Skriv ut varje unikt ord på en egen rad, och ange hur ofta ordet förekommer(ex "och (2)")
+         if
+        //Skriv ut varje unikt ord på en egen rad, sorterat efter hur ofta ordet förekommer
 
 
-                        OrdKollen[Compare] = +1;
-
-                    }
-                }
-            }
 
 
-            Console.Read();
+
         }
     }
 }
