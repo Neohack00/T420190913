27e671ccc07206b4c54cf1beae7410b0ff00fe3b
Neohack00 <neo@bylock.se>
2019-09-19 14:30:34 +0200

    Preparing for new solution at Uppgift 2

--- Uppgifter/Uppgift 2/Program.cs
+++ Uppgifter/Uppgift 2/Program.cs
@@ -15,55 +15,31 @@ namespace Uppgift_2
             ////Låt användaren mata in en mening.
             Console.WriteLine("skriv en mening");
             string mening = Console.ReadLine();
-            string[] words = mening.Split(' ');
+            mening.Trim(' ', ',');
+            string[] inputwords = mening.Split(' ');
             //Skriv ut varje ord på en egen rad, i storleksordning(längd på ordet)
+            System.Collections.ArrayList words = new System.Collections.ArrayList();
+            foreach (string ord in inputwords)
+            {
+                
+                words.Add(ord);
+            }
+
+            ;
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            {
+                
+
+                //Skriv ut varje ord på en egen rad, i omvänd ordning(baklänges)
+                Console.Read();
+        }
+ 
+        
+
            
-            int MaxLen = 0;
-            int MinstLen = 0;
-            
-            foreach (string ord in words)
-            {
-
-                if (MaxLen < ord.Length)
-                {
-                    MaxLen = ord.Length;
-
-                    if (MinstLen == 0 )
-                    {
-                        MinstLen = MaxLen;
-                    }
-                }
-                else if(ord.Length < MinstLen)
-                {
-                    MinstLen = ord.Length;
-                }
-            }
-            string[] Sortedwords = new string[words.Length];
-
-            foreach(string ord in words)
-            {
-                if (MaxLen == ord.Length)
-                {
-                    Sortedwords[0] = ord;
-                }
-                else if(MinstLen == ord.Length)
-                {
-                    Sortedwords[words.Length - 1] =  ord;
-                }
-            
-
-            }
-
-            Console.WriteLine("Störst ordvärde är {0} och minsta är {1}", MaxLen, MinstLen);
-            foreach(string ord in words)
-            {
-                Console.WriteLine("{0} : ({1})", ord , ord.Length);
-            }
-            
-
-
-            //Skriv ut varje ord på en egen rad, i omvänd ordning(baklänges)
-            Console.Read();
         }
     }
 }
