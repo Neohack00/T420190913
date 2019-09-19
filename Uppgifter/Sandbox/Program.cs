cd3b1d8aea37298097cf75cc52940aae108198b2
Neohack00 <neo@bylock.se>
2019-09-19 16:22:33 +0200

    Testing 2D array: Failed

--- Uppgifter/Sandbox/Program.cs
+++ Uppgifter/Sandbox/Program.cs
@@ -12,21 +12,42 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-         ArrayList Alist = new ArrayList();
-            Alist.Add("Hej");
-            Alist.Add("Neo");
-            Alist.Add("!");
+            //ArrayList Alist = new ArrayList();
+            //   Alist.Add("Hej");
+            //   Alist.Add("Neo");
+            //   Alist.Add("!");
 
-            Console.WriteLine(Alist);
-            Console.WriteLine("Alist");
-            Console.WriteLine(" Count:  {0}", Alist.Count);
-            Console.WriteLine(" Capacity:   {0} ", Alist.Capacity);
-            Console.WriteLine("Values:    ");
-            foreach(Object Sak in Alist)
+            //   Console.WriteLine(Alist);
+            //   Console.WriteLine("Alist");
+            //   Console.WriteLine(" Count:  {0}", Alist.Count);
+            //   Console.WriteLine(" Capacity:   {0} ", Alist.Capacity);
+            //   Console.WriteLine("Values:    ");
+            //   foreach(Object Sak in Alist)
+            //   {
+            //       Console.WriteLine("    {0}", Sak);
+            //   }
+            //   Console.ReadLine();
+
+            int y = 0;
+            int[,] TwoD = new int[4, 4];
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine("    {0}", Sak);
+                for (int j = 0; j < 4; j++)
+                {
+                    TwoD[i, j] = y;
+                    y++;
+                }
+
             }
-            Console.ReadLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; i < 3; j++)
+                {
+                    Console.WriteLine("Row {0} Column{1} = {2}", i, j, TwoD[i, j]);
+              
+                }
+            }
+            Console.Read();
 
         }
     }
